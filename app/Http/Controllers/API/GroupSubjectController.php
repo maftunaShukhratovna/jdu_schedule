b1aad997fc9e42f2361f2211441955eebdb8fc97
maftunaShukhratovna <brightstarmaftuna@gmail.com>
2025-02-25 06:17:49 +0500

    role user and completed crud

--- app/Http/Controllers/API/GroupSubjectController.php
+++ app/Http/Controllers/API/GroupSubjectController.php
@@ -1,10 +1,48 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
+use App\Models\Group;
 use Illuminate\Http\Request;
 
 class GroupSubjectController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validator = $request->validate([
+            'group_id' => 'required|integer|exists:groups,id',
+            'subject_id' => 'required|integer|exists:subjects,id',
+        ]);
+
+        $group = Group::findOrFail($validator['group_id']);
+        $group->subjects()->attach($validator['subject_id']);
+
+        return response()->json(['message' => 'Subject added to group']);
+    }
+
+    public function update(Request $request)
+    {
+        $validator = $request->validate([
+            'group_id' => 'required|integer|exists:groups,id',
+            'subject_id' => 'required|integer|exists:subjects,id',
+        ]);
+
+        $group = Group::findOrFail($validator['group_id']);
+        $group->subjects()->sync($validator['subject_id']);
+
+        return response()->json(['message' => 'Subject updated in group']);
+    }
+
+    public function destroy(string $id, Request $request)
+    {
+        $validator = $request->validate([
+            'group_id' => 'required|integer|exists:groups,id',
+        ]);
+
+        $group = Group::findOrFail($validator['group_id']);
+        $group->subjects()->detach($id);
+
+        return response()->json(['message' => 'Subject removed from group']);
+    }
 }
--- app/Http/Controllers/API/RoleUserController.php
+++ app/Http/Controllers/API/RoleUserController.php
@@ -1,10 +1,53 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleUserController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(User::with('roles')->get());
+    }
+    public function store(Request $request)
+    {
+        $validator = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'role_id' => 'required|exists:roles,id',
+        ]);
+
+        $user = User::findOrFail($validator['user_id']);
+        $user->roles()->attach($validator['role_id']);
+
+        return response()->json(['message' => 'Role assigned to user successfully.']);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $validator = $request->validate([
+            'role_id' => 'required|array',
+            'role_id.*' => 'exists:roles,id',
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->roles()->sync($validator['role_id']);
+
+        return response()->json(['message' => 'User roles updated successfully.']);
+    }
+
+    public function destroy(Request $request, string $id)
+    {
+        $validator = $request->validate([
+            'role_id' => 'required|exists:roles,id',
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->roles()->detach($validator['role_id']);
+
+        return response()->json(['message' => 'User role removed successfully.']);
+    }
 }

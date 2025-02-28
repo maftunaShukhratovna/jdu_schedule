d89170d818916d926ac29ba77eb5f5026d504769
maftunaShukhratovna <brightstarmaftuna@gmail.com>
2025-02-28 05:56:59 +0500

    validated request

--- app/Http/Requests/StoreRoleUserRequest.php
+++ app/Http/Requests/StoreRoleUserRequest.php
@@ -11,7 +11,7 @@ class StoreRoleUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class StoreRoleUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'role_id' => 'required|integer|exists:roles,id',
+            'user_id' => 'required|integer|exists:users,id',
         ];
     }
 }
